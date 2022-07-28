4dc77d214f3734bcf3613736d5ea37b7e794d8ad
Diego C. Silva <diegocaetano444@outlook.com>
2022-07-28 04:07:19 -0300

    [🎲] - implemented method for medlewares(request interceptor)

--- src/app.js
+++ src/app.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const app = express();
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 app.use(express.json());
 
 const obras = [];
+
+// Middlewares - interceptador de requisições
+function logRequest(request, response, next) {
+    console.time(request.method +" - "+ request.url)
+    next();
+    console.timeEnd(request.method +" - "+ request.url);
+}
+
+function checkId(request, response, next) {
+    const { id } = request.params;
+
+    if(!isUuid(id)) {
+        return response.status(400).json({ message: "Project not found" });
+    }
+    next();
+}
+
+app.use(logRequest);
+app.use("/obras/:id", checkId);
 
 app.get("/obras", function(request, response) {
     return response.json(obras);
 });
 
 app.post("/obras", function(request, response) {
-    const { id, titulo, editora, foto, autores } = request.body;
+    const { titulo, editora, foto, autores } = request.body;
     const NovaObra = {
         id: uuid(),
         titulo: titulo,
@@ -42,12 +61,12 @@ app.put("/obras/:id", function(request, response) {
     return response.json(UppObra)
 });
 
-app.delete("/obras/:id", function(request, response) {
+app.delete("/obras/:id", checkId, function(request, response) {
     const { id } = request.params;
     
     const indexObra = obras.findIndex(function(obra) { obra.id == id });
     obras.splice(indexObra, 1);
-    
+
     return response.status(204).send();
 });
 
