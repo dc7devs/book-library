d150a17069aeabbf536d2bd2b56ccfcf5e94f01b
Diego C. Silva <diegocaetano444@outlook.com>
2022-08-15 12:36:06 -0300

    [🎲] implemented post route

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,65 @@
 import express from 'express';
 import { uuid } from 'uuidv4';
 
-export const routes = express.Router();
-const obras = [];
+interface Author {
+    name: string;
+}
+ 
+interface LiteraryBookProps {
+    id: string;
+    title: string;
+    publishingCompany: string;
+    photo?: string;
+    authors: Author[];
+}
 
-routes.get('/obras', (req, res) => {
-    return res.json(obras);
+export const routes = express.Router();
+const Libry:Array<LiteraryBookProps> = [];
+
+routes.get('/literary-books', (req, res) => {
+    return res.json(Libry);
 });
 
-routes.post('/obras', (req, res) => {});
+routes.post('/literary-books', (req, res) => {
+    const { 
+        title, 
+        publishingCompany, 
+        photo, 
+        authors
+    } : LiteraryBookProps = req.body;
 
-routes.put('/obras/:id', (req, res) => {});
+    const newLiteraryBook : LiteraryBookProps = {
+        id: uuid(),
+        title,
+        publishingCompany,
+        photo,
+        authors
+    }
 
-routes.delete('/obras/:id', (req, res) => {});
+    Libry.push(newLiteraryBook);
+    return res.status(201).send(newLiteraryBook);
+});
+
+routes.put('/literary-books/:id', (req, res) => {
+    const { id } = req.params;
+    const { 
+        title, 
+        publishingCompany, 
+        photo, 
+        authors
+    } : LiteraryBookProps = req.body;
+
+    const updateLiteraryBook : LiteraryBookProps = {
+        id,
+        title,
+        publishingCompany,
+        photo,
+        authors
+    }
+
+    const indexBook = Libry.findIndex(book => book.id === id);
+
+    Libry[indexBook] = updateLiteraryBook;
+
+    return res.json(updateLiteraryBook);
+});
